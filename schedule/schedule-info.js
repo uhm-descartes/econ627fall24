f0c97837202e19ce5d1834a805e0125569f77033
ntarui <ntarui@users.noreply.github.com>
2023-11-13 07:12:01 +0000

    published by GitHub Actions

---
+++ schedule/schedule-info.js
@@ -0,0 +1,16 @@
+moreaEventData = [
+  {title: "A02: Assess your ability to write effectively about open source software", url: "/econ627fall24/morea/example-open-source-software/assessment-open-source-software.html", start: "2021-07-23T09:00"},
+  {title: "E07: Reflect on smart questions", url: "/econ627fall24/morea/example-open-source-software/experience-smart-questions.html", start: "2021-07-22T23:00"},
+  {title: "E05: isUnique", url: "/econ627fall24/morea/example-javascript/experience-ctci.html", start: "2021-07-14T23:00"},
+  {title: "A01: Assessment of simple Javascript skills", url: "/econ627fall24/morea/example-javascript/assessment-javascript-1.html", start: "2021-07-16T09:00"},
+  {title: "E06: Reflect on Javascript 1", url: "/econ627fall24/morea/example-javascript/experience-javascript-1-technical-essay.html", start: "2021-07-15T23:00"},
+  {title: "E04: Introduction to Javascript with FreeCodeCamp", url: "/econ627fall24/morea/example-javascript/experience-interactive-javascript-1.html", start: "2021-07-13T23:00"},
+  {title: "E03: Learn the command line", url: "/econ627fall24/morea/Introduction/experience-learn-the-command-line.html", start: "2021-07-09T23:00"},
+  {title: "E02: Introduce yourself", url: "/econ627fall24/morea/Introduction/experience-introduce-yourself.html", start: "2021-07-08T23:00"},
+  {title: "Introduction to ECON 627", url: "/econ627fall24/modules/introduction", start: "2024-08-26", end: "2024-12-15T23:00"},
+  {title: "E01: Environment configuration", url: "/econ627fall24/morea/Introduction/experience-environment-configuration.html", start: "2021-07-07T23:00"},
+  {title: "E09: Doing good as a software engineer", url: "/econ627fall24/morea/example-ethics/experience-se-ethics-good.html", start: "2021-07-27T23:00"},
+  {title: "E10: Doing harm as a software engineer", url: "/econ627fall24/morea/example-ethics/experience-se-ethics-harm.html", start: "2021-07-28T23:00"},
+  {title: "E08: What are the professional codes of software engineering ethics?", url: "/econ627fall24/morea/example-ethics/experience-se-ethics-codes.html", start: "2021-07-26T23:00"},
+  {title: "A03: Assess your ability to reason about ethical issues in software development", url: "/econ627fall24/morea/example-ethics/assessment-ethics.html", start: "2021-07-30T09:00"}
+]
